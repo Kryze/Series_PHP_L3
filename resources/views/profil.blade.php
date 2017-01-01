af1a621ebe1e55d013ad3fed248303aa9313880c
Corentin Legris <corentin.legris9@etu.univ-lorraine.fr>
2017-01-01 21:52:34 +0100

    Css pas fini, push du profil

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -2,11 +2,32 @@
 @section('title', 'Profil')
 @section('content')
 	<?php
-	 echo "<div id=infos><h1 id=nomprofil>Bonjour, $user->name</h1>
-		  <h2> Email : $user->email</div> 
-		  <p> Episodes Visionnées : </p>";
-		  foreach ($userepisodes as $episode){
-		  echo "<p>$episode->episode_id</p>";
-		  }
+	 echo "
+<div class='well saut_ligne'>
+    <h2 id='profil'>Profil</h2>
+
+    <div class='well' id='infos'>
+          <h4><B>Nom :</B> $user->name</>
+		  <h4><B>Email :</B> $user->email</div>
+	</div>
+</div>
+
+<div class='well saut_ligne2'>
+    <h2 id='profil2'>Série(s) regardée(s)</h2>
+
+    <div class='well' id='infos'>
+         <p> Episodes Visionnées : </p>";
+    foreach ($seasonsepisodes as $season) {
+        $state = DB::table('seasons')->where('id',$season->season_id)->orderBy('seasons.number', 'asc')->get();
+        foreach ($state as $s) {
+            echo "<p>$season->name</p>";
+        }
+    }
+    foreach ($userepisodes as $episode){
+        echo "<p>$episode->episode_id</p>";
+    }
+    echo"
+	</div>
+</div>";
 	?>
 @endsection
